ee9e684c0ab781990f4a4fb572c8ed768632bd50
danielf-05 <danielfgayoso@gmail.com>
2026-02-01 13:53:41 +0100

    logic change

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ import utils
 def main():
     
     print("HASHING APP TEST")
-
-    if(len(sys.argv)<=1):
-        print("Not enough args")
-        return
-
-    passwd = str(sys.argv[1])
-    hashed_passwd = utils.encrypt(passwd)
-
-    hashed_passwd_json = utils.buildJson(passwd, hashed_passwd)
-    print("Your hashed passwd is: " + hashed_passwd)
-    print(hashed_passwd_json)
+    utils.execute()
     
 main()
--- utils.py
+++ utils.py
@@ -1,18 +1,51 @@
-from Crypto.Hash import SHA256
-import json
+from cryptography.fernet import Fernet
 
-def encrypt(passwd: str):
-    m = SHA256.new()
-    encodedPasswd = passwd.encode()
-    m.update(encodedPasswd)
-    m.digest()
-    hashed_passwd = m.hexdigest()
-    return hashed_passwd
+def generate_key() -> Fernet:
+    key = Fernet.generate_key()
+    print("key generated -> ", key)
+    fKey = Fernet(key)
+    save_key(key)
+    return fKey
 
-def buildJson(original_passwd: str, hashed_passwd: str):
-    preJson = {
-        "original_passwd": original_passwd,
-        "hashed_passwd": hashed_passwd
-    }
-    jsons = json.dumps(preJson)
-    return jsons
+def encrypt_message(message, fKey: Fernet) -> bytes:
+    token = fKey.encrypt(bytes(message.encode()))
+    print("Message encrypted using key and stored in 'token' variable ->", token)
+    return token
+    
+def decrypt_message(token: bytes) -> str:
+    key = load_key()
+    fKey = Fernet(key)
+    return fKey.decrypt(token).decode()
+
+def ask_user_message() -> any:
+    message = input("Introduce the message to encrypt and decrypt: ")
+    return message
+
+# EXECUTE
+def execute():
+   fernetKey = generate_key()
+   token = encrypt_message(ask_user_message(), fernetKey)
+   message = decrypt_message(token)
+   print("The decrypted message is:", message)
+    
+    
+    
+def save_key(key):
+    key_file = open('secret.key', 'wb')
+    if(key_file.write(key) <= 0):
+        print("Error writing the file")
+        return
+        
+    key_file.close()
+    print("Key file has been generated successfully")
+    
+def load_key(file_loc="./"): 
+    key_file = open('secret.key', 'rb')
+    key = key_file.read()
+    key_len = len(key)
+    if(key_len <= 0):
+        print("Error reading the file")
+        return
+    
+    key_file.close()
+    return key
